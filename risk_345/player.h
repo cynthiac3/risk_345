18ecedb11fba881af44a0f77c3c9f16571ac4e4f
Cynthia <cynthia.cherrier@hotmail.com>
2018-09-20 15:08:49 -0400

    Player class

---
+++ risk_345/player.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Player {
+
+	/* ATTRIBUTES:
+		-collection of countries that the player owns
+		-hand of risk cards
+		-dice rolling facility
+	 */
+
+	void reinforce();
+
+	void attack();
+
+	void fortify();
+
+	// Driver
+	int main() {
+		return 0;
+	}
+
+};
