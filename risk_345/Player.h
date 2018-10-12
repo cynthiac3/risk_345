05697b16037ed7cd45114fa24a38875c9ff75b5a
cynthiac3 <cynthia.cherrier@hotmail.com>
2018-10-12 16:07:27 -0400

    Update Player.h

--- risk_345/Player.h
+++ risk_345/Player.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "Map_CPNT.h"
-#include "dice.h"
+#include "Dice.h"
+#include "Hand.h"
+
+
 
 class Player {
-
-	/* Attributes */
-	// collection of countries that the player owns
-	vector<Territorie> myTerritories;
-	// A dice rolling facility
+	// Attributes
+	vector<Territorie*> myTerritories;
 	Dice dice;
-	// Hand of risk card
-	//... 	
+	Hand hand;
 
 public: 
 	Player();
 	Dice getDice(); // returns the dice facility the player owns
+	void getCountries(); // returns a string that list the countries info 
+	void getHand(); // returns the hand of cards of the player
 	// Methods to implement in later assignments:
 	void reinforce();
 	void attack();
