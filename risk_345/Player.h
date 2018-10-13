a901bbfd98ea0a12bbe0510b167d6a127cc0eb4c
cynthiac3 <cynthia.cherrier@hotmail.com>
2018-10-12 22:01:41 -0400

    Update Player.h
    changed Hand object for a vector of cards

--- risk_345/Player.h
+++ risk_345/Player.h
@@ -1,25 +1,25 @@
 #pragma once
 #include "Map_CPNT.h"
 #include "Dice.h"
-#include "Hand.h"
+#include "Deck.h"
 
 
 class Player {
 	// Attributes
 	vector<Territory*> myTerritories; // a collection of countries the player owns
 	Dice dice; // a dice rolling facility
-	Hand hand; // a hand of risk cards
+	vector<Deck::Cards> handOfCards; // a collection of cards that the player holds
 
 public: 
-	Player();
+	Player(); // default constructor
 	Dice getDice(); // returns the dice facility the player owns
 	void getCountries(); // returns a string that list the countries info 
-	Hand getHand(); // returns the hand of cards of the player
+	vector<Deck::Cards> getHand(); // returns the hand of cards of the player
+	void setHand(Deck deck); // method to add cards to the vector handOfCards
+
 	// Methods to implement in later assignments:
 	void reinforce();
 	void attack();
 	void fortify();
 
 };
-
-
