ccdcc0d6b83be44486b27540f0e6800a707ca8c7
cynthiac3 <cynthia.cherrier@hotmail.com>
2018-11-01 15:24:53 -0400

    Update Player.h
    reflect changes in Player.cpp

--- risk_345/Player.h
+++ risk_345/Player.h
@@ -2,20 +2,22 @@
 #include "Map_CPNT.h"
 #include "Dice.h"
 #include "Deck.h"
+#include "Hand.h"
 
 
 class Player {
 	// Attributes
 	vector<Territory*> myTerritories; // a collection of countries the player owns
 	Dice dice; // a dice rolling facility
-	vector<Deck::Cards> handOfCards; // a collection of cards that the player holds
+	Hand hand; // hand that holds the cards
 
 public: 
 	Player(); // default constructor
+	Player(Deck *deck);
 	Dice getDice(); // returns the dice facility the player owns
 	void getCountries(); // prints the names of the countries owned 
-	vector<Deck::Cards> getHand(); // returns the hand of cards of the player
-	void setHand(Deck deck); // method to add cards to the vector handOfCards
+	Hand* getHand(); // return hand of cards the player owns
+	void setHand(Deck *deck); // method to add cards to the vector handOfCards
 
 	// Methods to implement in later assignments:
 	void reinforce();
