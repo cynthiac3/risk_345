85da2d07a54fff21db35877354002ed812c014e8
CogNoman <cognoman@hotmail.com>
2018-11-30 17:31:06 -0500

    Update Graphics.h

--- Graphics.h
+++ Graphics.h
@@ -3,3 +3,21 @@
 //free functions
 void riskLogo();
 void empty(int lines);
+void space(int lines);
+
+void bigGame();
+void bigOne();
+void bigTwo();
+void bigThree();
+void bigFour();
+void bigFive();
+
+// If you want to "animate" the game ending, these methods build up the numbers line by line
+void bigGameScrollUp();
+void bigOneScrollUp();
+void bigTwoScrollUp();
+void bigThreeScrollUp();
+void bigFourScrollUp();
+void bigFiveScrollUp();
+
+void mapBig();
