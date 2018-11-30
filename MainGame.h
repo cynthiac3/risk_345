f3ea0712ee3394b7668f8b85592b42f919c4886d
CogNoman <cognoman@hotmail.com>
2018-11-30 04:46:10 -0500

    Move free functions to header

--- MainGame.h
+++ MainGame.h
@@ -1,11 +1,8 @@
 #pragma once
+#include "Player.h"
+#include <vector>
 
-class MainGame {
-
-public:
-
-	//The following methods should be in the header
-	//void run();
-	//void clearInputs();
-	//void checkPlayersEliminated(vector<Player*> * players); // This line here causes some problems if it's in the header because of <vector>
-};
+// free functions declaration
+void runSingleGame();
+void clearInputs();
+void checkPlayersEliminated(vector<Player*> * players); 
